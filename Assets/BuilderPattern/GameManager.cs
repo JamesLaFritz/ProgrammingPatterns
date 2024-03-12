eadd39e61209e7e536f511ac57355a130143cce4
James LaFritz <ktmarine1999+git@gmail.com>
2024-03-12 16:00:23 -0400

    feat(builderpattern): Add GameManager
    Add GameManager inorder to show how to use the builder pattern.

---
+++ Assets/BuilderPattern/GameManager.cs
@@ -0,0 +1,25 @@
+﻿#region Header
+// GameManager.cs
+// 
+// From [Warped Imagination](https://www.youtube.com/@WarpedImagination)
+#endregion
+
+using UnityEngine;
+
+namespace BuilderPattern
+{
+    public class GameManager : MonoBehaviour
+    {
+        private void Start()
+        {
+            Enemy enemy = new Enemy.FluentBuilder()
+                .WithName("Goblin")
+                .WithHealth(100)
+                .WithSpeed(5f)
+                .WithDamage(10)
+                .Build();
+
+            Instantiate(enemy);
+        }
+    }
+}
