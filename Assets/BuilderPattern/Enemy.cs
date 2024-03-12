9d16362f26469d964f14aa9951f9db4df9d887e2
James LaFritz <ktmarine1999+git@gmail.com>
2024-03-12 16:06:38 -0400

    feat(builderpattern): implement fluent builder pattern for Enemy class
    Replace telescoping constructors with a fluent builder pattern in the Enemy class to improve code readability and maintainability.

--- Assets/BuilderPattern/Enemy.cs
+++ Assets/BuilderPattern/Enemy.cs
@@ -12,20 +12,63 @@ namespace BuilderPattern
 		public int Damage { get; private set; }
 		public bool IsBoss { get; private set; }
 
-
-		// Code smell
-		// Too many constructor parameters
-		// Telescoping Constructors
-
-		public Enemy(string name) => Name = name;
-		public Enemy(string name, int health) : this(name) => Health = health;
-		public Enemy(string name, int health, float speed) : this(name, health) => Speed = speed;
-		public Enemy(string name, int health, float speed, int damage) : this(name, health, speed) => Damage = damage;
-
-		public Enemy(string name, int health, float speed, int damage, bool isBoss) : this(name, health, speed) =>
-			IsBoss = isBoss;
-
 		// Do Enemy Stuff
+
+		/// <summary>
+		/// A fluent builder that is easier to read.
+		/// Stores all of the properties required to build an enemy and has a method that returns itself every time you set one of the properties.
+		/// Finally have a build method to create a new enemy.
+		/// </summary>
+		public class FluentBuilder
+		{
+			private string m_name;
+			private int m_health;
+			private float m_speed;
+			private int m_damage;
+			private bool m_isBoss;
+
+			public FluentBuilder WithName(string name)
+			{
+				m_name = name;
+				return this;
+			}
+
+			public FluentBuilder WithHealth(int health)
+			{
+				m_speed = health;
+				return this;
+			}
+
+			public FluentBuilder WithSpeed(float speed)
+			{
+				m_speed = speed;
+				return this;
+			}
+
+			public FluentBuilder WithDamage(int damage)
+			{
+				m_damage = damage;
+				return this;
+			}
+
+			public FluentBuilder WithIsBoss(bool isBoss)
+			{
+				m_isBoss = isBoss;
+				return this;
+			}
+
+			public Enemy Build()
+			{
+				var enemy = new GameObject("Enemy").AddComponent<Enemy>();
+				enemy.Name = m_name;
+				enemy.Health = m_health;
+				enemy.Speed = m_speed;
+				enemy.Damage = m_damage;
+				enemy.IsBoss = m_isBoss;
+
+				return enemy;
+			}
+		}
 	}
 }
 
